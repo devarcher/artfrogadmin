52db11485567bdb9fa7a59b023a1170e46832cbc
devarcher <archershot@hotmail.com>
2020-02-02 18:22:59 -0600

    teachers state successfully mapped to list item

---
+++ src/Components/TeacherCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeacherCard = props => {
+  const { teachers } = this.props;
+
+  console.log('teacherCard:', teachers);
+
+  return <div>this is teacher card</div>;
+};
+
+export default TeacherCard;
--- src/Components/Teachers.js
+++ src/Components/Teachers.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import TeacherCard from './TeacherCard';
 
 class Teachers extends React.Component {
   state = {
-    teachers: null
+    teachers: []
   };
 
   componentDidMount() {
@@ -14,24 +15,30 @@ class Teachers extends React.Component {
       const url = 'http://localhost:80/adminEditTeacher';
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      // console.log(data);
       const teacherData = data.teachers.map(teacher => ({
         id: teacher.id,
-        fist_name: teacher.first_name,
+        first_name: teacher.first_name,
         last_name: teacher.last_name,
         bio: teacher.bio
       }));
       console.log(teacherData);
-      this.setState({ teacher: teacherData });
+      this.setState({ teachers: teacherData });
     } catch (err) {
       console.log(err);
     }
   }
 
   render() {
+    const { teachers } = this.state;
+    console.log('in render: ', teachers);
     return (
       <div>
-        <div>Teachers</div>
+        <ul>
+          {teachers.map(teacher => (
+            <li key={teacher.id}>{teacher.first_name}</li>
+          ))}
+        </ul>
       </div>
     );
   }
