dbb903cc1df95c40696c38d1f14984c17b66dd5a
devarcher <archershot@hotmail.com>
2020-02-05 16:02:51 -0600

    began working on edit mode on form

--- src/Components/Teachers/TeacherCard.js
+++ src/Components/Teachers/TeacherCard.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Typography, CssBaseline, Button, Box } from '@material-ui/core';
+import {
+  Typography,
+  CssBaseline,
+  Button,
+  Box,
+  TextField
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
@@ -53,98 +59,196 @@ const TeacherCard = props => {
             </Box>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <form className={classes.form} noValidate autoComplete="off">
-              <Box
-                p={1}
-                display="flex"
-                alignItems="center"
-                borderBottom="1px solid grey"
-              >
-                <Box marginRight="10px">
-                  <Typography variant="h6">
-                    <strong>First Name: </strong>
-                  </Typography>
-                </Box>
-                <Box flexGrow={1} textAlign="left">
-                  <Typography variant="h6">{teachers.first_name}</Typography>
-                </Box>
-                <Box flexGrow={1} textAlign="right">
-                  <Button
-                    variant="outlined"
-                    color="secondary"
-                    startIcon={<EditIcon />}
-                    onClick={changeEditMode}
-                  >
-                    Edit
-                  </Button>
-                </Box>
-              </Box>
-
-              <Box
-                p={1}
-                display="flex"
-                alignItems="center"
-                borderBottom="1px solid grey"
-              >
-                <Box marginRight="10px">
-                  <Typography variant="h6">
-                    <strong>Last Name: </strong>
-                  </Typography>
-                </Box>
-                <Box>
-                  <Typography variant="h6" flexGrow={1} textAlign="left">
-                    {teachers.last_name}
-                  </Typography>
-                </Box>
-                <Box flexGrow={1} textAlign="right">
-                  <Button
-                    variant="outlined"
-                    color="secondary"
-                    startIcon={<EditIcon />}
-                    onClick={changeEditMode}
-                  >
-                    Edit
-                  </Button>
-                </Box>
-              </Box>
-
-              <Box
-                p={1}
-                display="flex"
-                alignItems="center"
-                borderBottom="1px solid grey"
-              >
-                <Box marginRight="10px">
-                  <Typography variant="h6">
-                    <strong>Bio: </strong>
-                  </Typography>
-                </Box>
-                <Box variant="h6" flexGrow={1} textAlign="left" padding={4}>
-                  <Typography>{teachers.bio}</Typography>
-                </Box>
-                <Box flexGrow={1} textAlign="right">
-                  <Button
-                    variant="outlined"
-                    color="secondary"
-                    startIcon={<EditIcon />}
-                    onClick={changeEditMode}
-                  >
-                    Edit
-                  </Button>
-                </Box>
-              </Box>
-              <Box padding={2}>
-                <Button
-                  size="large"
-                  variant="contained"
-                  color="secondary"
-                  startIcon={<DeleteIcon />}
-                  className={classes.deleteButton}
+            {isInEditMode ? (
+              <form className={classes.form} noValidate autoComplete="off">
+                <Box
+                  p={1}
+                  display="flex"
+                  alignItems="center"
+                  borderBottom="1px solid grey"
                 >
-                  Delete {teachers.first_name} {teachers.last_name}
-                </Button>
-              </Box>
-            </form>
+                  <Box marginRight="10px">
+                    <Typography variant="h6">
+                      <strong>First Name: </strong>
+                    </Typography>
+                  </Box>
+                  <Box flexGrow={1} textAlign="left">
+                    <TextField
+                      id="outlined-basic"
+                      label="First Name"
+                      variant="outlined"
+                      onKeyUp={event => onKeyUp(event)}
+                      autoFocus={true}
+                      value={teachers.first_name}
+                    />
+                  </Box>
+                  <Box flexGrow={1} textAlign="right">
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<EditIcon />}
+                      onClick={changeEditMode}
+                    >
+                      Edit
+                    </Button>
+                  </Box>
+                </Box>
+                <Box
+                  p={1}
+                  display="flex"
+                  alignItems="center"
+                  borderBottom="1px solid grey"
+                >
+                  <Box marginRight="10px">
+                    <Typography variant="h6">
+                      <strong>Last Name: </strong>
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <Typography variant="h6" flexGrow={1} textAlign="left">
+                      {teachers.last_name}
+                    </Typography>
+                  </Box>
+                  <Box flexGrow={1} textAlign="right">
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<EditIcon />}
+                      onClick={changeEditMode}
+                    >
+                      Edit
+                    </Button>
+                  </Box>
+                </Box>
+                <Box
+                  p={1}
+                  display="flex"
+                  alignItems="center"
+                  borderBottom="1px solid grey"
+                >
+                  <Box marginRight="10px">
+                    <Typography variant="h6">
+                      <strong>Bio: </strong>
+                    </Typography>
+                  </Box>
+                  <Box variant="h6" flexGrow={1} textAlign="left" padding={4}>
+                    <Typography>{teachers.bio}</Typography>
+                  </Box>
+                  <Box flexGrow={1} textAlign="right">
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<EditIcon />}
+                      onClick={changeEditMode}
+                    >
+                      Edit
+                    </Button>
+                  </Box>
+                </Box>
+                <Box padding={2}>
+                  <Button
+                    size="large"
+                    variant="contained"
+                    color="secondary"
+                    startIcon={<DeleteIcon />}
+                    className={classes.deleteButton}
+                  >
+                    Delete {teachers.first_name} {teachers.last_name}
+                  </Button>
+                </Box>
+              </form>
+            ) : (
+              <form className={classes.form} noValidate autoComplete="off">
+                <Box
+                  p={1}
+                  display="flex"
+                  alignItems="center"
+                  borderBottom="1px solid grey"
+                >
+                  <Box marginRight="10px">
+                    <Typography variant="h6">
+                      <strong>First Name: </strong>
+                    </Typography>
+                  </Box>
+                  <Box flexGrow={1} textAlign="left">
+                    <Typography variant="h6">{teachers.first_name}</Typography>
+                  </Box>
+                  <Box flexGrow={1} textAlign="right">
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<EditIcon />}
+                      onClick={changeEditMode}
+                    >
+                      Edit
+                    </Button>
+                  </Box>
+                </Box>
+                <Box
+                  p={1}
+                  display="flex"
+                  alignItems="center"
+                  borderBottom="1px solid grey"
+                >
+                  <Box marginRight="10px">
+                    <Typography variant="h6">
+                      <strong>Last Name: </strong>
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <Typography variant="h6" flexGrow={1} textAlign="left">
+                      {teachers.last_name}
+                    </Typography>
+                  </Box>
+                  <Box flexGrow={1} textAlign="right">
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<EditIcon />}
+                      onClick={changeEditMode}
+                    >
+                      Edit
+                    </Button>
+                  </Box>
+                </Box>
+                <Box
+                  p={1}
+                  display="flex"
+                  alignItems="center"
+                  borderBottom="1px solid grey"
+                >
+                  <Box marginRight="10px">
+                    <Typography variant="h6">
+                      <strong>Bio: </strong>
+                    </Typography>
+                  </Box>
+                  <Box variant="h6" flexGrow={1} textAlign="left" padding={4}>
+                    <Typography>{teachers.bio}</Typography>
+                  </Box>
+                  <Box flexGrow={1} textAlign="right">
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<EditIcon />}
+                      onClick={changeEditMode}
+                    >
+                      Edit
+                    </Button>
+                  </Box>
+                </Box>
+                <Box padding={2}>
+                  <Button
+                    size="large"
+                    variant="contained"
+                    color="secondary"
+                    startIcon={<DeleteIcon />}
+                    className={classes.deleteButton}
+                  >
+                    Delete {teachers.first_name} {teachers.last_name}
+                  </Button>
+                </Box>
+              </form>
+            )}
           </ExpansionPanelDetails>
         </ExpansionPanel>
 
