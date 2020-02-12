3e0d6970d5eff8ba41ce2f9e8e840c9da0ff4eb1
devarcher <archershot@hotmail.com>
2020-02-11 21:22:08 -0600

    added all column names to studentsData fetch

--- src/Components/Students/StudentsData.js
+++ src/Components/Students/StudentsData.js
@@ -23,7 +23,20 @@ class StudentsData extends React.Component {
       const studentsData = data.students.map(student => ({
         id: student.id,
         first_name: student.first_name,
-        last_name: student.last_name
+        last_name: student.last_name,
+        guardian_first_name: student.guardian_first_name,
+        guardian_last_name: student.guardian_last_name,
+        student_DOB: student.student_DOB,
+        phone: student.phone,
+        email: student.email,
+        address: student.address,
+        city: student.city,
+        state: student.state,
+        zip: student.zip,
+        alt_first_name: student.alt_first_name,
+        alt_last_name: student.alt_last_name,
+        alt_phone: student.alt_phone,
+        photo_permission: student.photo_permission
       }));
       // console.log(studentsData);
       this.setState({ students: studentsData });
