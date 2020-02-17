2e538fc2a8a98a834e7f8de70fe4ed68a0e3f713
devarcher <archershot@hotmail.com>
2020-02-17 14:31:19 -0600

    added error for too few letters

--- src/Components/SingleComponents/SearchField.js
+++ src/Components/SingleComponents/SearchField.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import { TextField, Box, Button } from '@material-ui/core';
@@ -18,29 +18,58 @@ export default function SearchField(props) {
   const classes = useStyles();
   const { searchFieldText, query, onSearchSubmit } = props;
 
+  const [error, setError] = useState(false);
+
+  const clickHandler = () => {
+    if (query.length < 3) {
+      setError(true);
+    } else {
+      setError(false);
+      onSearchSubmit();
+    }
+  };
+
   return (
     <div>
       <Box className={classes.root} display="flex" align="center" width="75vw">
-        <TextField
-          fullWidth
-          variant="outlined"
-          placeholder="Enter Query"
-          value={query}
-          onChange={e => searchFieldText(e)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            )
-          }}
-        />
+        {error ? (
+          <TextField
+            fullWidth
+            error
+            value={query}
+            helperText="Enter at least 3 letters."
+            variant="outlined"
+            onChange={e => searchFieldText(e)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              )
+            }}
+          />
+        ) : (
+          <TextField
+            fullWidth
+            variant="outlined"
+            placeholder="Search By Name"
+            value={query}
+            onChange={e => searchFieldText(e)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              )
+            }}
+          />
+        )}
         <Button
           variant="contained"
           color="primary"
           size="large"
           className={classes.subButton}
-          onClick={onSearchSubmit}
+          onClick={clickHandler}
         >
           Submit
         </Button>
